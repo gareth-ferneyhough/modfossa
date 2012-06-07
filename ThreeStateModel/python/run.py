094fc9c996ef6210bf88a96c8163384bfcdd6853
Gareth Ferneyhough <gareth00@gmail.com>
2012-06-07 15:20:22 +0000

    Added plotting functionality in 'run.py'. Changed some build settings to use 8 cores instead of 16.

--- ThreeStateModel/python/run.py
+++ ThreeStateModel/python/run.py
@@ -1,7 +1,21 @@
 import numpy
-import libModFossa
 import pyublas
+import matplotlib.pyplot as plt
+import libModFossa
 
 
 mat = libModFossa.solve()
-print mat
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(mat[:,0])
+ax.plot(mat[:,1])
+ax.plot(mat[:,2])
+
+leg = ax.legend(('C1', 'C2', 'O'), 'center right', shadow=True)
+ax.set_xlabel('Time (ms)')
+ax.set_ylabel('Probability')
+ax.set_title('Channel Probability')
+
+plt.show()
+
